2c4bf0038a909c474ac691862b0bbfb5c4040ca3
Frank <f.neumann.berlin@googlemail.com>
2023-04-03 08:57:36 +0200

    Erster Eintrag mit Funktion

--- app/src/main/java/com/example/simplemenus/MainActivity.kt
+++ app/src/main/java/com/example/simplemenus/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.simplemenus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import com.example.simplemenus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar :Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        toolbar = binding.mytoolbar.root
+        toolbar.title = "Meine App mit Menü"
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when(item.itemId){
+            R.id.action_settings -> {
+                Toast.makeText(this, "Settings wurde getippt", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
