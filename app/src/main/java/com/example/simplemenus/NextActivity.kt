e1e023401c0a5eff522561fef0192cf6e53f6c52
Frank <f.neumann.berlin@googlemail.com>
2023-04-04 08:17:29 +0200

    mit ContextMenu

--- app/src/main/java/com/example/simplemenus/NextActivity.kt
+++ app/src/main/java/com/example/simplemenus/NextActivity.kt
@@ -1,11 +1,34 @@
 package com.example.simplemenus
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.simplemenus.databinding.ActivityNextBinding
 
 class NextActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNextBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_next)
+
+        binding = ActivityNextBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.tvOutput.setOnCreateContextMenuListener { menu, view, menuInfo ->
+            menuInflater.inflate(R.menu.menu_color,menu)
+        }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.action_red -> binding.root.setBackgroundColor(Color.RED)
+            R.id.action_green -> binding.root.setBackgroundColor(Color.GREEN)
+        }
+
+        return true
     }
 }
